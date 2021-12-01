26294d5d192e3c4aa6bd7c70d74826e5f0ca2b65
a19336136 <aaaksenova@gmail.com>
2021-12-01 13:05:41 +0300

    new

--- detect.py
+++ detect.py
@@ -72,11 +72,11 @@ def highlight_bad_words(text):
         doc = model(sent)
         for w in doc:
             if w.lemma_ in stop_words.keys():
-                text = re.sub(w.text, '''<span class="words" style="background-color: rgba(255, 255, 128, .5)">''' + w.text + '</span>', text)
+                text = re.sub(w.text, '''<a class="words" style="background-color: rgba(255, 255, 128, .5)">''' + w.text + '</a>', text)
     for stop_phrase in stop_phrases.split(', '):
-        text = re.sub(stop_phrase, '''<span class="words" style="background-color: rgba(255, 255, 128, .5)">''' + stop_phrase + '</span>', text)
+        text = re.sub(stop_phrase, '''<a class="words" style="background-color: rgba(255, 255, 128, .5)">''' + stop_phrase + '</a>', text)
         text = re.sub(stop_phrase.capitalize(), \
-                      '''<span class="words" style="background-color: rgba(255, 255, 128, .5)">''' + stop_phrase.capitalize() + '</span>', text)
+                      '''<a class="words" style="background-color: rgba(255, 255, 128, .5)">''' + stop_phrase.capitalize() + '</a>', text)
     return text
 
 
