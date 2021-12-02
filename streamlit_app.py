1545e3c65e2efd1aa8b24ec9eead179265891679
a19336136 <aaaksenova@gmail.com>
2021-12-02 12:28:08 +0300

    new

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,7 @@ st.sidebar.markdown('''<span style="color:red">Таким</span> – сложн
                     unsafe_allow_html=True)
 st.sidebar.markdown('''<span style="color:orange">Таким</span> – сложные конструкции существительных \n''',
                     unsafe_allow_html=True)
-st.sidebar.markdown("**Важно!** Этот интструмент лишь подсказывает возможные ошибки, но не гарантирует их наличие.")
+st.sidebar.markdown("**Важно!** Этот инструмент лишь подсказывает возможные ошибки, но не гарантирует их наличие.")
 st.sidebar.markdown("С замечаниями и предложениями писать Анне Аксеновой *tg: aksenysh*")
 
 with st.form(key='my_form'):
