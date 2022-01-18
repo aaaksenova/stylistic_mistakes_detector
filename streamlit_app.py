d876280aef461ea01947baadeedc70a2ab7d2942
a19336136 <aaaksenova@gmail.com>
2022-01-18 15:27:27 +0300

    minor fixes

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,8 @@ import streamlit as st
 import pandas as pd
 
 
-st.title('Главред для НРМ')
+title = '<p style="font-family-sans-serif; color:Green; font-size: 42px;">СберГлавред</p>'
+st.markdown(title, unsafe_allow_html=True)
 st.sidebar.subheader("Это инструмент для выявления стилистических ошибок в тексте")
 st.sidebar.markdown('''<span style="color:green">Таким</span> цветом выделяются канцеляризмы и вводные слова \n''',
                     unsafe_allow_html=True)
